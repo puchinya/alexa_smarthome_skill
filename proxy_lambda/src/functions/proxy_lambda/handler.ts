e60ae225848c4f025894ca02a094e2e8b7607671
puchinya <puchinya@daradara.jp>
2022-07-31 20:23:07 +0900

    correct

--- proxy_lambda/src/functions/proxy_lambda/handler.ts
+++ proxy_lambda/src/functions/proxy_lambda/handler.ts
@@ -1,31 +1,33 @@
 import 'source-map-support/register';
 import {
     LambdaClient,
-    InvokeCommand,
-    InvocationRequest,
-    InvocationResponse
+    InvokeCommand
 } from "@aws-sdk/client-lambda";
 
 const FUNCTION_NAME : string = process.env.FUNCTION_NAME;
 const FUNCTION_REGION : string = process.env.FUNCTION_REGION;
 
+let lambda_client = null;
+
 const proxy = async (event, context) : Promise<string> => {
-    const client = new LambdaClient({
-        region: FUNCTION_REGION
-    });
+    if(lambda_client == null) {
+        lambda_client = new LambdaClient({
+            region: FUNCTION_REGION
+        });
+    }
 
-    console.log(event);
+    //console.log(event);
 
-    const ret = await client.send(new InvokeCommand(
+    const ret = await lambda_client.send(new InvokeCommand(
         {
             FunctionName: FUNCTION_NAME,
             Payload: Uint8Array.from(Buffer.from(JSON.stringify(event)))
         }
     ));
 
-    console.log(ret);
+    //console.log(ret);
     const payload = Buffer.from(ret.Payload).toString();
-    console.log(payload);
+    //console.log(payload);
 
     return JSON.parse(payload);
 };
