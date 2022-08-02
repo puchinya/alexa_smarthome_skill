5744395fda36f31aa4fc36a2040bc92e316b951c
puchinya <puchinya@daradara.jp>
2022-08-03 07:22:51 +0900

    correct

--- src/functions/smarthome/handler.ts
+++ src/functions/smarthome/handler.ts
@@ -25,7 +25,41 @@ function log(message, message1, message2) {
   console.log(message + message1 + message2);
 }
 
-async function handleAuthorization(request, context) {
+interface AlexaSmartHomeSkillEventHeader {
+  namespace: string;
+  name: string;
+  messageId: string;
+  payloadVersion: string;
+  correlationToken: string;
+}
+
+interface AlexaSmartHomeSkillDirectiveEndpointScope {
+  type: string;
+  token: string;
+}
+
+interface AlexaSmartHomeSkillEventEndpoint {
+  scope: AlexaSmartHomeSkillDirectiveEndpointScope;
+  endpointId: string;
+  cookie: object;
+}
+
+interface AlexaSmartHomeSkillRequestEvent {
+  header: AlexaSmartHomeSkillEventHeader;
+  endpoint: AlexaSmartHomeSkillEventEndpoint;
+  payload: any;
+}
+
+interface AlexaSmartHomeSkillRequest {
+  directive: AlexaSmartHomeSkillRequestEvent;
+}
+
+interface AlexaSmartHomeSkillResponse {
+  event: AlexaSmartHomeSkillRequestEvent;
+  context: any;
+}
+
+async function handleAuthorization(request, context) : Promise<any> {
   // Send the AcceptGrant response
   let payload = {};
   const header = request.directive.header;
@@ -35,151 +69,153 @@ async function handleAuthorization(request, context) {
   const jwt = decodeJwt(requestToken);
   const uid : string = jwt.sub;
 
-  const code = request["directive"]["payload"]["grant"]["code"];
+  const code = request.directive.payload.grant.code;
 
   await issueLwaTokenByCodeAndSaveToDb(uid, code);
 
   log("DEBUG", "AcceptGrant Response: ", JSON.stringify({ header: header, payload: payload }));
-  context.succeed({ event: { header: header, payload: payload } });
+
+  return { event: { header: header, payload: payload } };
+}
+
+function handleDiscovery(request, context) : any {
+  // Send the discovery response
+  const payload = {
+    "endpoints":
+        [
+          {
+            "endpointId": "sample-bulb-01",
+            "manufacturerName": "Smart Device Company",
+            "friendlyName": "Livingroom lamp",
+            "description": "Virtual smart light bulb",
+            "displayCategories": ["LIGHT"],
+            "additionalAttributes":  {
+              "manufacturer" : "Sample Manufacturer",
+              "model" : "Sample Model",
+              "serialNumber": "U11112233456",
+              "firmwareVersion" : "1.24.2546",
+              "softwareVersion": "1.036",
+              "customIdentifier": "Sample custom ID"
+            },
+            "cookie": {
+              "key1": "arbitrary key/value pairs for skill to reference this endpoint.",
+              "key2": "There can be multiple entries",
+              "key3": "but they should only be used for reference purposes.",
+              "key4": "This is not a suitable place to maintain current endpoint state."
+            },
+            "capabilities":
+                [
+                  {
+                    "interface": "Alexa.PowerController",
+                    "version": "3",
+                    "type": "AlexaInterface",
+                    "properties": {
+                      "supported": [{
+                        "name": "powerState"
+                      }],
+                      "proaciveReport": true,
+                      "retrievable": true,
+                    }
+                  },
+                  {
+                    "type": "AlexaInterface",
+                    "interface": "Alexa.EndpointHealth",
+                    "version": "3.2",
+                    "properties": {
+                      "supported": [{
+                        "name": "connectivity"
+                      }],
+                      "retrievable": true
+                    }
+                  },
+                  {
+                    "type": "AlexaInterface",
+                    "interface": "Alexa",
+                    "version": "3"
+                  }
+                ]
+          }
+        ]
+  };
+  const header = request.directive.header;
+  header.name = "Discover.Response";
+  log("DEBUG", "Discovery Response: ", JSON.stringify({ header: header, payload: payload }));
+  return { header: header, payload: payload };
+}
+
+function handlePowerControl(request: AlexaSmartHomeSkillRequest, context) : any {
+  // get device ID passed in during discovery
+  const requestMethod = request.directive.header.name;
+  const responseHeader = request.directive.header;
+  responseHeader.namespace = "Alexa";
+  responseHeader.name = "Response";
+  responseHeader.messageId = responseHeader.messageId + "-R";
+  // get user token pass in request
+  const requestToken = request.directive.endpoint.scope.token;
+  let powerResult;
+
+  if (requestMethod === "TurnOn") {
+
+    // Make the call to your device cloud for control
+    // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
+    powerResult = "ON";
+  }
+  else if (requestMethod === "TurnOff") {
+    // Make the call to your device cloud for control and check for success
+    // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
+    powerResult = "OFF";
+  }
+  // Return the updated powerState.  Always include EndpointHealth in your Alexa.Response
+  const contextResult = {
+    "properties": [{
+      "namespace": "Alexa.PowerController",
+      "name": "powerState",
+      "value": powerResult,
+      "timeOfSample": "2017-09-03T16:20:50.52Z", //retrieve from result.
+      "uncertaintyInMilliseconds": 50
+    },
+      {
+        "namespace": "Alexa.EndpointHealth",
+        "name": "connectivity",
+        "value": {
+          "value": "OK"
+        },
+        "timeOfSample": "2022-03-09T22:43:17.877738+00:00",
+        "uncertaintyInMilliseconds": 0
+      }]
+  };
+  const response = {
+    context: contextResult,
+    event: {
+      header: responseHeader,
+      endpoint: {
+        scope: {
+          type: "BearerToken",
+          token: requestToken
+        },
+        endpointId: "sample-bulb-01"
+      },
+      payload: {}
+    }
+  };
+  log("DEBUG", "Alexa.PowerController ", JSON.stringify(response));
+  return response;
 }
 
 const smarthome = async (request, context) : Promise<any> => {
   if (request.directive.header.namespace === 'Alexa.Discovery' && request.directive.header.name === 'Discover') {
     log("DEBUG:", "Discover request",  JSON.stringify(request));
-    handleDiscovery(request, context);
+    return handleDiscovery(request, context);
   }
   else if (request.directive.header.namespace === 'Alexa.PowerController') {
     if (request.directive.header.name === 'TurnOn' || request.directive.header.name === 'TurnOff') {
       log("DEBUG:", "TurnOn or TurnOff Request", JSON.stringify(request));
-      handlePowerControl(request, context);
+      return handlePowerControl(request, context);
     }
   }
-  else if (request.directive.header.namespace === 'Alexa.Authorization' && request.directive.header.name === 'AcceptGrant') {
-    await handleAuthorization(request, context)
-  }
-
-  function handleDiscovery(request, context) {
-    // Send the discovery response
-    var payload = {
-      "endpoints":
-          [
-            {
-              "endpointId": "sample-bulb-01",
-              "manufacturerName": "Smart Device Company",
-              "friendlyName": "Livingroom lamp",
-              "description": "Virtual smart light bulb",
-              "displayCategories": ["LIGHT"],
-              "additionalAttributes":  {
-                "manufacturer" : "Sample Manufacturer",
-                "model" : "Sample Model",
-                "serialNumber": "U11112233456",
-                "firmwareVersion" : "1.24.2546",
-                "softwareVersion": "1.036",
-                "customIdentifier": "Sample custom ID"
-              },
-              "cookie": {
-                "key1": "arbitrary key/value pairs for skill to reference this endpoint.",
-                "key2": "There can be multiple entries",
-                "key3": "but they should only be used for reference purposes.",
-                "key4": "This is not a suitable place to maintain current endpoint state."
-              },
-              "capabilities":
-                  [
-                    {
-                      "interface": "Alexa.PowerController",
-                      "version": "3",
-                      "type": "AlexaInterface",
-                      "properties": {
-                        "supported": [{
-                          "name": "powerState"
-                        }],
-                        "proaciveReport": true,
-                        "retrievable": true,
-                      }
-                    },
-                    {
-                      "type": "AlexaInterface",
-                      "interface": "Alexa.EndpointHealth",
-                      "version": "3.2",
-                      "properties": {
-                        "supported": [{
-                          "name": "connectivity"
-                        }],
-                        "retrievable": true
-                      }
-                    },
-                    {
-                      "type": "AlexaInterface",
-                      "interface": "Alexa",
-                      "version": "3"
-                    }
-                  ]
-            }
-          ]
-    };
-    var header = request.directive.header;
-    header.name = "Discover.Response";
-    log("DEBUG", "Discovery Response: ", JSON.stringify({ header: header, payload: payload }));
-    context.succeed({ event: { header: header, payload: payload } });
-  }
-
-  function handlePowerControl(request, context) {
-    // get device ID passed in during discovery
-    var requestMethod = request.directive.header.name;
-    var responseHeader = request.directive.header;
-    responseHeader.namespace = "Alexa";
-    responseHeader.name = "Response";
-    responseHeader.messageId = responseHeader.messageId + "-R";
-    // get user token pass in request
-    var requestToken = request.directive.endpoint.scope.token;
-    var powerResult;
-
-    if (requestMethod === "TurnOn") {
-
-      // Make the call to your device cloud for control
-      // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-      powerResult = "ON";
-    }
-    else if (requestMethod === "TurnOff") {
-      // Make the call to your device cloud for control and check for success
-      // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-      powerResult = "OFF";
-    }
-    // Return the updated powerState.  Always include EndpointHealth in your Alexa.Response
-    var contextResult = {
-      "properties": [{
-        "namespace": "Alexa.PowerController",
-        "name": "powerState",
-        "value": powerResult,
-        "timeOfSample": "2017-09-03T16:20:50.52Z", //retrieve from result.
-        "uncertaintyInMilliseconds": 50
-      },
-        {
-          "namespace": "Alexa.EndpointHealth",
-          "name": "connectivity",
-          "value": {
-            "value": "OK"
-          },
-          "timeOfSample": "2022-03-09T22:43:17.877738+00:00",
-          "uncertaintyInMilliseconds": 0
-        }]
-    };
-    var response = {
-      context: contextResult,
-      event: {
-        header: responseHeader,
-        endpoint: {
-          scope: {
-            type: "BearerToken",
-            token: requestToken
-          },
-          endpointId: "sample-bulb-01"
-        },
-        payload: {}
-      }
-    };
-    log("DEBUG", "Alexa.PowerController ", JSON.stringify(response));
-    context.succeed(response);
+  else if (request.directive.header.namespace === 'Alexa.Authorization' &&
+      request.directive.header.name === 'AcceptGrant') {
+    return await handleAuthorization(request, context)
   }
 };
 
