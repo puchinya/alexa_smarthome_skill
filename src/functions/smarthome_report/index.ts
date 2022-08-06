94862ec5bcb6df1955f31e730e77d27c47e2bb0f
puchinya <puchinya@daradara.jp>
2022-08-06 10:55:10 +0900

    correct

--- src/functions/smarthome_report/index.ts
+++ src/functions/smarthome_report/index.ts
@@ -10,8 +10,8 @@ export default {
     architecture: Architecture.arm64,
     memorySize: 256,
     environment: {
-        LWA_CLIENT_ID: "amzn1.application-oa2-client.e7b1e99a38be41428d8b1072cf075d92",
-        LWA_CLIENT_SECRET: "d7db2db893c48967797a5fc9f9c9a5986624f771e7659ee8095bd5bfd3a73f7c"
+        LWA_CLIENT_ID: "",
+        LWA_CLIENT_SECRET: ""
     },
     events: [
     ]
